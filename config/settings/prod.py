47a1454158746bae7de65726b437753ec25ab33f
ticleless <kyy20802@gmail.com>
2022-12-26 21:42:48 +0900

    add domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ['3.36.213.108']
+ALLOWED_HOSTS = ['3.36.213.108', 'rollingball.be']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
